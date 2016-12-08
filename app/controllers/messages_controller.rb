c83f32e04db1857bd8b10b35b03fa3686fa69cf1
raraose <lovebyrara@icloud.com>
2016-12-08 03:42:12 +0000

    create message model

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,7 +1,11 @@
 class MessagesController < ApplicationController
   def index
-    @message = Message.new
+     @message = Message.new
+    # Messageを全て取得する。
+    @messages = Message.all
   end
+  # 中略
+  
 
   def create
     @message = Message.new(message_params)
